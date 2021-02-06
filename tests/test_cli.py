6bbb92064e02beaaf5e620912913edbcae67ddbd
Jay Qi <jayqi@users.noreply.github.com>
2021-02-06 00:26:46 -0800

    Add tests for python -m CLI invocation

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,10 @@
 import filecmp
+import subprocess
 
 from typer.testing import CliRunner
 
+import erdantic as erd
 from erdantic.cli import app
-from erdantic.erd import draw, to_dot
 from erdantic.examples.pydantic import Party
 from erdantic.version import __version__
 
@@ -12,20 +13,41 @@ runner = CliRunner()
 
 
 def test_draw(tmp_path):
+    # With library for comparison
+    path_base = tmp_path / "diagram_base.png"
+    erd.draw(Party, out=path_base)
+    assert path_base.exists()
+
+    # With CLI
     path1 = tmp_path / "diagram1.png"
     result = runner.invoke(app, ["erdantic.examples.pydantic.Party", "-o", str(path1)])
     assert result.exit_code == 0
     assert path1.exists()
+    assert filecmp.cmp(path1, path_base)
 
-    path2 = tmp_path / "diagram2.png"
-    draw(Party, out=path2)
-    assert path2.exists()
-
-    assert filecmp.cmp(path1, path2)
-
+    # Overwrite error
     result = runner.invoke(app, ["erdantic.examples.pydantic.Quest", "-o", str(path1)])
     assert result.exit_code == 1
-    assert filecmp.cmp(path1, path2)
+    assert filecmp.cmp(path1, path_base)
+
+    # Overwrite
+    result = runner.invoke(
+        app, ["erdantic.examples.pydantic.Quest", "-o", str(path1), "--overwrite"]
+    )
+    assert result.exit_code == 0
+    assert not filecmp.cmp(path1, path_base)
+
+    # python -m erdantic
+    path2 = tmp_path / "diagram2.png"
+    result = subprocess.run(
+        ["python", "-m", "erdantic", "erdantic.examples.pydantic.Party", "-o", str(path2)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    assert result.returncode == 0
+    assert path2.exists()
+    assert filecmp.cmp(path2, path_base)
 
 
 def test_dot(tmp_path):
@@ -33,7 +55,18 @@ def test_dot(tmp_path):
     result = runner.invoke(app, ["erdantic.examples.pydantic.Party", "-d", "-o", str(path)])
     assert result.exit_code == 0
     assert not path.exists()  # -o is ignored and no file created
-    assert to_dot(Party) in result.stdout
+    assert erd.to_dot(Party).strip() == result.stdout.strip()
+
+    # python -m erdantic
+    result = subprocess.run(
+        ["python", "-m", "erdantic", "erdantic.examples.pydantic.Party", "-d", "-o", str(path)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    assert result.returncode == 0
+    assert not path.exists()  # -o is ignored and no file created
+    assert erd.to_dot(Party).strip() == result.stdout.strip()
 
 
 def test_help():
