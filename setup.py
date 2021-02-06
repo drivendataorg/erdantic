0e63ed78f6b34fe3606676fdbf00277741a61f79
Jay Qi <jayqi@users.noreply.github.com>
2021-02-06 00:33:25 -0800

    Add Python 3.9 to CI

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description=("Draw entity relation diagrams for Pydantic models using GraphViz."),
     entry_points={"console_scripts": ["erdantic=erdantic.cli:app"]},
