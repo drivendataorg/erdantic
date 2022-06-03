91beca6bc54ceb9ebb1b3fa995acd60067427af7
Peter Bull <pjbull@gmail.com>
2022-06-03 13:18:44 -0700

    Add python extra for mkdocstrings (#56)
    * add python extra for mkdocstrings
    
    * Drop support for python 3.6
    
    * Apply suggestions from code review
    
    Co-authored-by: Jay Qi <2721979+jayqi@users.noreply.github.com>

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # erdantic Changelog
 
+## v0.5.0 (2022-06-03)
+
+- Removed support for Python 3.6. ([Issue #51](https://github.com/drivendataorg/erdantic/issues/51), [PR #56](https://github.com/drivendataorg/erdantic/pull/56))
+
 ## v0.4.1 (2022-04-08)
 
 - Fixed error when rendering a data model that has field using `typing.Literal`. ([PR #49](https://github.com/drivendataorg/erdantic/pull/49))
--- setup.py
+++ setup.py
@@ -25,17 +25,17 @@ requirements = load_requirements(Path(__file__).parent / "requirements.txt")
 setup(
     author="DrivenData",
     author_email="info@drivendata.org",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Code Generators",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description=("Entity relationship diagrams for Python data model classes like Pydantic."),
     entry_points={"console_scripts": ["erdantic=erdantic.cli:app"]},
@@ -51,5 +51,5 @@ setup(
         "Source Code": "https://github.com/drivendataorg/erdantic",
     },
     url="https://github.com/drivendataorg/erdantic",
-    version="0.4.1",
+    version="0.5.0",
 )
