e7127546792dff4755ea175a2eda7bb4305022b4
Jay Qi <2721979+jayqi@users.noreply.github.com>
2024-04-10 11:16:34 -0400

    Fix ModuleNotFoundError if attrs is not installed when importing examples (#113)
    * Fix ModuleNotFoundError if attrs is not installed
    
    * Bump version
    
    ---------
    
    Co-authored-by: Jay Qi <jayqi@users.noreply.github.com>

--- erdantic/examples/__init__.py
+++ erdantic/examples/__init__.py
@@ -1,10 +1,18 @@
 """Example data model classes."""
 
-from erdantic.examples import attrs, dataclasses, pydantic, pydantic_v1
+from erdantic.examples import dataclasses, pydantic, pydantic_v1
 
 __all__ = [
-    "attrs",
     "dataclasses",
     "pydantic",
     "pydantic_v1",
 ]
+
+try:
+    from erdantic.examples import attrs
+
+    attrs
+    __all__.append("attrs")
+except ModuleNotFoundError as e:
+    if e.name != "attrs":
+        raise
