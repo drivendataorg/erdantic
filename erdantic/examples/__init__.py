279008595889f42d2e3c4dca01dce8f22c48d594
Jay Qi <jayqi@users.noreply.github.com>
2026-02-15 14:28:37 -0500

    Fix examples imports

--- erdantic/examples/__init__.py
+++ erdantic/examples/__init__.py
@@ -2,17 +2,18 @@
 
 import sys
 
-from erdantic.examples import dataclasses, msgspec, pydantic
+from erdantic.examples import dataclasses, pydantic
+
+__all__ = [
+    "dataclasses",
+    "pydantic",
+]
 
 if sys.version_info < (3, 14):
     from erdantic.examples import pydantic_v1
 
-__all__ = [
-    "dataclasses",
-    "msgspec",
-    "pydantic",
-    "pydantic_v1",
-]
+    pydantic_v1
+    __all__.append("pydantic_v1")
 
 try:
     from erdantic.examples import attrs
@@ -21,4 +22,13 @@ try:
     __all__.append("attrs")
 except ModuleNotFoundError as e:
     if e.name != "attrs":
+        raise
+
+try:
+    from erdantic.examples import msgspec
+
+    msgspec
+    __all__.append("msgspec")
+except ModuleNotFoundError as e:
+    if e.name != "msgspec":
         raise
