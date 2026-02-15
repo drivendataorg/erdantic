43d6e7f474b13231aa144e08b2b0a3a41c161063
Jay Qi <jayqi@users.noreply.github.com>
2026-02-15 00:18:33 -0500

    Update test to use subdirectory

--- tests/test_against_assets.py
+++ tests/test_against_assets.py
@@ -12,8 +12,12 @@ import erdantic.core
 import erdantic.examples
 from tests.utils import assert_dot_equals
 
-ASSETS_DIR = Path(__file__).resolve().parent / "assets"
+if sys.version_info < (3, 14):
+    ASSETS_SUBDIR = "py_lt_314"
+else:
+    ASSETS_SUBDIR = "py_gte_314"
 
+ASSETS_DIR = Path(__file__).resolve().parent / "assets" / ASSETS_SUBDIR
 
 CASES = [
     ("attrs", erdantic.examples.attrs),
