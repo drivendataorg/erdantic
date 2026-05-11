e59729b5bb8922f30b65e762b70cfa7a38c7672b
Jay Qi <2721979+jayqi@users.noreply.github.com>
2026-05-10 21:29:44 -0400

    Fix tests related to Pydantic docstring changes (#169)
    * Add 7-day dependency cooldown
    
    * Update dependencies and regenerate test assets
    
    * Fix type error
    
    * Remove outdated typing ignore
    
    ---------
    
    Co-authored-by: Jay Qi <jayqi@users.noreply.github.com>

--- erdantic/exceptions.py
+++ erdantic/exceptions.py
@@ -1,5 +1,5 @@
 import sys
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
     from erdantic.core import FullyQualifiedName
@@ -36,13 +36,12 @@ class UnresolvableForwardRefError(NameError, ErdanticException):
     def __init__(
         self,
         *args: object,
-        name: str,
+        name: Optional[str],
         model_full_name: "FullyQualifiedName",
     ) -> None:
         self.model_full_name = model_full_name
         if sys.version_info >= (3, 10):
-            # typeshed is wrong; NameError does have keyword argument 'name'
-            super().__init__(*args, name=name)  # type: ignore [call-arg]
+            super().__init__(*args, name=name)
         else:
             super().__init__(*args)
             self.name = name
